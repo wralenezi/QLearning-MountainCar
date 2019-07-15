327a7e61f13c9085a785c2a93622181a97094cd5
wralenezi <wralenezi@gmail.com>
2019-07-14 23:23:08 -0400

    Updated

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
 import gym
+import numpy as np
 import qtable_train as ql
+
+SHOW_EVERY = 2000
+EPISODES = 25000
 
 # Set the environment we want to work with
 env = gym.make("MountainCar-v0")
 
 
 # Set the DQN for training 
-#dqn_model = ql.QTableModel(env.observation_space.high,env.observation_space.low,env.observation_space.high,env.action_space.n)
+dqn_model = ql.QTableModel(len(env.observation_space.high),env.observation_space.low,env.observation_space.high,env.action_space.n)
 
-# Reset the environment 
-env.reset()
+# Learning Loop
+for episode in range(EPISODES):
+	if episode % SHOW_EVERY == 0:
+		render = True
+	else:
+		render = False
+	# Reset the environment 
+	discrete_state = dqn_model.get_discrete_state(env.reset())
+	done = False
 
-done = False
+	# Loop through the environment
+	while not done:
+		# choose the action based on the highest q value
+		action = np.argmax(dqn_model.get_qvalues_for_state(discrete_state))
+		# make the action and receive the outcomes
+		new_state, reward, done, _ = env.step(action)
+		# get the new state and discretize it
+		new_discrete_state = dqn_model.get_discrete_state(new_state)
+		if render:
+			# render the environment 
+			env.render()
+		# Get the 
+		if not done:
+			# get the max q value for the new discrete state
+			max_future_q = np.max(dqn_model.get_qvalues_for_state(new_discrete_state))
+			# Get the current q value for the action taken
+			current_q = dqn_model.get_qvalues_for_state(discrete_state)[action]
+			# Calculate the new q value for that instance
+			new_q = dqn_model.get_new_qvalue(current_q,max_future_q,reward)
+			# assign it on the q table
+			dqn_model.set_qvalue_on_qtable(discrete_state,action,new_q)
+		# Setting the reward or rather the max q value at the instance
+		elif new_state[0] >= env.goal_position:
+			print(f"Solved at {episode}")
+			dqn_model.set_qvalue_on_qtable(discrete_state,action,0)		
 
-# Loop through the environment
-while not done:
-	action = 2
-	# make the action and recive the outcomes
-	new_state, reward, done, _ = env.step(action)
-	# render the enviroment 
-	env.render()
+		discrete_state = new_discrete_state
 
-env.close()
+		env.close()
--- qtable_train.py
+++ qtable_train.py
@@ -1,27 +1,42 @@
 import numpy as np
 
 
-# Hyperparameters
+# Hyper-parameters
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95
 EPISODES = 25000
+# the number of bins for the state space; the more it is the larger the Q table is
+STATE_SLOTS = 20
 
 
-# The main trainer for QLearning Algorithm for a an agent with continous state
+# The main trainer for QLearning Algorithm for a an agent with continuous state
 # obs_dimension: the dimension of observational state  
 # observations_low: array of the lowest values of the state space
 # observations_highest: array of the highest values of the state space
 # action_space: the number of actions 
 class QTableModel:
 	def __init__(self,obs_dimension,observations_low,observations_high,action_num):
-		# Observation space
-		self.observation_space_size =  [20] * obs_dimension
-		# Size of bin for co ntinuous observation, should be the dimension of the state
+		self.observations_low = observations_low
+		# Observation space 
+		self.observation_space_size =  [STATE_SLOTS] * obs_dimension
+		# Size of bin for co continuous observation, should be the dimension of the state
 		self.observation_bin_size = (observations_high - observations_low)/self.observation_space_size
-		# Initialize Q table to store the action-state with the Q values, the highest is the goal reward
-		self.q_table  = np.random.uniform(low=-2, high=0, size = (self.observation_space_size + action_num))
+		# Initialize Q table to store the action-state with the Q values, the highest is the goal reward		
+		self.q_table  = np.random.uniform(low=-2, high=0, size = (self.observation_space_size + [action_num]))		
 
-	# discretize a continous state to the defined bins
-	def get_discrete_state(self,state,observations_low):
-		discrete_state = (state - observations_low) / self.observation_bin_size
+	# discretize a continuous state to the defined bins
+	def get_discrete_state(self,state):
+		discrete_state = (state - self.observations_low) / self.observation_bin_size
+		# return the state in a tuple as integer
 		return tuple(discrete_state.astype(np.int))
+
+	def get_qvalues_for_state(self,state):
+		return self.q_table[state]
+
+	def get_new_qvalue(self,current_q,max_future_q,reward):
+		new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)		
+		return new_q
+
+	def set_qvalue_on_qtable(self,discrete_state,action,new_q):
+		self.q_table[discrete_state + (action, )] = new_q
+
